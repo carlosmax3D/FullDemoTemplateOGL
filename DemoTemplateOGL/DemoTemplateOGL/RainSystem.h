28daca2b059c07728cc7a0210ba91a7066ff372f
Carlos Israel Orta Orta <carlos.orta@coreapps.mx>
2025-10-03 10:58:52 -0600

    Added rain

---
+++ DemoTemplateOGL/DemoTemplateOGL/RainSystem.h
@@ -0,0 +1,49 @@
+#pragma once
+#include "Base/Utilities.h"
+#include "Base/camera.h"
+#include "Base/shader.h"
+#include "Terreno.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <vector>
+
+struct RainDrop {
+    glm::vec3 position;
+    glm::vec3 velocity;
+};
+
+struct Splash {
+    glm::vec3 position;
+    float life;   // remaining time
+    int active = 0;
+    float size;
+};
+
+class RainSystem {
+public:
+    RainSystem(int maxDrops, float areaSize, float height, Camera *cameraDetails);
+
+    void update(Terreno *t);
+    void render();
+
+private:
+    std::vector<RainDrop> drops;
+    std::vector<Splash> splashes;
+    Camera *cam;
+
+    unsigned int dropVAO, dropVBO;
+    unsigned int splashVAO, splashVBO;
+
+    int maxDrops;
+    float areaSize;
+    float height;
+
+    Shader *dropShader = NULL;
+    Shader *splashShader = NULL;
+    unsigned int dropTexture;
+    unsigned int splashTexture;
+
+    void initGL();
+    void respawn(RainDrop& drop);
+    void spawnSplash(const glm::vec3& pos, int idx);
+};
